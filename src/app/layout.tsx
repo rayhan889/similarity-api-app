ad9fefd5fa6c8c7dab7f8d69214a75623f09e25b
rayhan889 <raydev1604@gmail.com>
2023-03-30 01:02:15 +0700

    add more space for mobile devices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
     >
       <body className="min-h-screen bg-slate-50 dark:bg-slate-900 antialiased">
         <Providers>{children}</Providers>
+
+        {/* mobile devices height */}
+        <div className="h-40 md:hidden" />
       </body>
     </html>
   );
