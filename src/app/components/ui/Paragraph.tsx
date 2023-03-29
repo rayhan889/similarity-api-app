8b2321947bae3a0c8bf5c4187720f91323d7343d
rayhan889 <raydev1604@gmail.com>
2023-03-29 22:38:47 +0700

    create a reusable paragraph component

---
+++ src/app/components/ui/Paragraph.tsx
@@ -0,0 +1,29 @@
+import { FC, HTMLAttributes, forwardRef } from "react";
+import { VariantProps, cva } from "class-variance-authority";
+
+const paragraphVariants = cva(
+  "max-w-prose text-slate-700 dark:text-slate-300 mb-2 text-center",
+  {
+    variants: {
+      size: {
+        default: "text-base sm:text-lg",
+        sm: "text-sm sm:text-base",
+      },
+    },
+    defaultVariants: {
+      size: "default",
+    },
+  }
+);
+
+interface ParagraphProps
+  extends HTMLAttributes<HTMLParagraphElement>,
+    VariantProps<typeof paragraphVariants> {}
+
+const Paragraph = forwardRef<HTMLParagraphElement, ParagraphProps>(
+  ({ className, size, children, ...props }, ref) => {
+    return <div ref={ref}>{children}</div>;
+  }
+);
+
+export default Paragraph;
