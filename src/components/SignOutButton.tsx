beedf9c3dd73a8ce8f22afd8f46a5fe4ec90d548
rayhan889 <raydev1604@gmail.com>
2023-03-30 11:52:44 +0700

    create sign in & out button components

---
+++ src/components/SignOutButton.tsx
@@ -0,0 +1,31 @@
+import { FC, useState } from "react";
+import Button from "@/ui/Button";
+import { signOut } from "next-auth/react";
+
+interface SignOutButtonProps {}
+
+const SignOutButton: FC<SignOutButtonProps> = ({}) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const signUserOut = async () => {
+    setIsLoading(true);
+
+    try {
+      await signOut();
+    } catch (error) {
+      //   toast({
+      //     title: "Error signing in",
+      //     message: "Please try again later",
+      //     type: "error",
+      //   });
+    }
+  };
+
+  return (
+    <Button onClick={signUserOut} isLoading={isLoading}>
+      Sign out
+    </Button>
+  );
+};
+
+export default SignOutButton;
