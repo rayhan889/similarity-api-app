361ce46e4bead4025a9eb48142f8e528d47ef2d2
rayhan889 <raydev1604@gmail.com>
2023-03-30 21:54:10 +0700

    config container, font, etc

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
@@ -10,7 +13,23 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "1.5rem",
+      screens: {
+        "2xl": "1360px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        ...colors,
+        "ligth-gold": "#f5bc51",
+        "dark-gold": "#533519",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("tailwindcss-animate")],
 };
