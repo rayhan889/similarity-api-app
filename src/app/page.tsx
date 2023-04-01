16610bce94c32d87c9318f799253a568147b6bf0
rayhan889 <raydev1604@gmail.com>
2023-04-01 21:15:23 +0700

    homepage finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,52 @@
+import Paragraph from "@/components/ui/Paragraph";
+import Heading from "@/components/ui/Heading";
 import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Similarity API | Home",
+  description: "Free & open-source text similarity API",
+};
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="font-bold text-red-600">Hello world!</h1>
+    <div className="h-screen relative flex items-center justify-center overflow-x-hidden">
+      <div className="container pt-32 max-w-7xl w-full mx-auto h-full">
+        <div className="h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start">
+          <Heading
+            size="lg"
+            className="heading-main text-black dark:text-ligth-gold"
+          >
+            Easily determine <br />
+            text similarity.
+          </Heading>
+          <Paragraph className="max-w-xl lg:text-left">
+            With the Text Similarity API, you can easliy determine the
+            similarity between two pieces of text with a free{" "}
+            <Link
+              href="/login"
+              className="underline underline-offset-2 text-black dark:text-ligth-gold"
+            >
+              API Key
+            </Link>
+            .
+          </Paragraph>
+
+          <div className="relative w-full max-w-lg lg:max-w-3xl aspect-square lg:absolute lg:left-1/2">
+            <Image
+              quality={100}
+              priority
+              fill
+              className="img-shadow"
+              style={{ objectFit: "contain" }}
+              src="/typewriter.png"
+              alt="Typewriter"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
