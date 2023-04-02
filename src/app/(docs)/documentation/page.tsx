abf48838116b1c0dfacf4e0ac273006917cc57ae
rayhan889 <raydev1604@gmail.com>
2023-04-02 20:08:50 +0700

    documentation page first init

---
+++ src/app/(docs)/documentation/page.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+
+import Heading from "@/ui/Heading";
+import Paragraph from "@/ui/Paragraph";
+import { FC } from "react";
+import DocumentationTabs from "@/components/DocumentationTabs";
+
+export const metadata: Metadata = {
+  title: "Similarity API | Documentation",
+  description: "Free & open-source text similarity API",
+};
+
+const page: FC = ({}) => {
+  return (
+    <div className="container max-w-7xl mx-auto pt-32">
+      <div className="flex flex-col items-center gap-6">
+        <Heading>Making a request.</Heading>
+        <Paragraph>api/v1/similarity</Paragraph>
+
+        <DocumentationTabs />
+      </div>
+    </div>
+  );
+};
+
+export default page;
