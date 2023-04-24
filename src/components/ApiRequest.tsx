5a085914106a47691f6402b64d716268ec2a851e
rayhan889 <raydev1604@gmail.com>
2023-04-24 18:56:26 +0700

    apiRequest component init

---
+++ src/components/ApiRequest.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface ApiRequestProps {
+  
+}
+
+const ApiRequest: FC<ApiRequestProps> = ({}) => {
+  return <div>ApiRequest</div>
+}
+
+export default ApiRequest
