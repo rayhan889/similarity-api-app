9a754326111018773550083f32d5309e3394600d
rayhan889 <raydev1604@gmail.com>
2023-04-24 18:55:58 +0700

    add few components in dashboard page

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,30 @@
-import { FC } from "react";
+import { Metadata } from "next";
+import { db } from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import ApiDashboard from "@/components/ApiDashboard";
+import ApiRequest from "@/components/ApiRequest";
 
-interface pageProps {}
+export const metadata: Metadata = {
+  title: "Similarity API | Dashboard",
+  description: "Free & open-source text similarity API",
+};
 
-const page: FC<pageProps> = ({}) => {
-  return <div>page</div>;
+const page = async ({}) => {
+
+  const user = await getServerSession(authOptions);
+  console.log(user?.user);
+
+  const apiKey = await db.apiKey.findFirst({
+    where: {userId : user?.user.id, enabled: true}
+  })
+  console.log('Api Key :' + apiKey);
+
+  return (
+    <div className="max-w-7xl mt-16 mx-auto">
+      {apiKey ? <ApiDashboard /> : <ApiRequest />}
+    </div>
+  );
 };
 
 export default page;
