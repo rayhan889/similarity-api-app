f71caf173ca0e73fc14face2f31b631c484ac793
rayhan889 <raydev1604@gmail.com>
2023-04-24 19:19:37 +0700

    make apiRequest function

--- src/components/ApiRequest.tsx
+++ src/components/ApiRequest.tsx
@@ -1,10 +1,39 @@
-import { FC } from 'react'
+import { FC, FormEvent } from 'react'
+import { useState } from 'react'
+import { toast } from './ui/Toast';
 
-interface ApiRequestProps {
-  
-}
+const ApiRequest:FC = ({}) => {
+    const [isCreating, setIsCreating] = useState<boolean>(false);
+    const [apiStr, setApiStr] = useState<string | null>(null);
 
-const ApiRequest: FC<ApiRequestProps> = ({}) => {
+    const createNewApiKey = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        setIsCreating(true);
+        try {
+            const generatedApiStr = await createApiKey();
+            setApiStr(generatedApiStr);
+        } catch (error) {
+            if(error instanceof Error){
+                toast({
+                    title: 'Error',
+                    message: error.message,
+                    type: 'error'
+                })
+
+                return
+            }
+
+            toast({
+                title: 'Error',
+                message: 'Something went wrong',
+                type: 'error'
+            })
+        } finally {
+            setIsCreating(false);
+        }
+    }
+
   return <div>ApiRequest</div>
 }
 
