f4058c11de99b1812ef3f6de37badcb3bcd37bf0
rayhan889 <raydev1604@gmail.com>
2023-05-08 13:00:30 +0700

    apiRequest type config

--- src/helpers/api-request.ts
+++ src/helpers/api-request.ts
@@ -2,5 +2,15 @@ import { CreateApiKey } from "../types/api-type";
 
 export const createApiKey = async () => {
     const response = await fetch('/api/api-key/create');
-    const data = (await response.json()) as CreateApiKey
+    const data = (await response.json()) as CreateApiKey;
+
+    if(data.error || !data.apiKey){
+        if(data.error instanceof Array){
+            throw new Error(data.error.join(' '))
+        }
+
+        throw new Error(data.error ?? 'Something went wrong');
+    }
+
+    return data.apiKey.key;
 }
