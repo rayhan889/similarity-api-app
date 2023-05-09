4c99bdbe1f623f8055caa5ada477982307bff599
rayhan889 <raydev1604@gmail.com>
2023-05-09 15:03:38 +0700

    adding style and props to input component

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,11 +1,24 @@
-import { FC } from 'react'
+import { FC, InputHTMLAttributes } from 'react'
+import React from 'react'
+import { cn } from '@/lib/utils'
 
-interface InputProps {
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   
 }
 
-const Input: FC<InputProps> = ({}) => {
-  return <div>Input</div>
-}
+const Input = React.forwardRef<HTMLInputElement, InputProps> (
+  ({className, ...props}, ref) => {
+    return (
+      <input
+        className={cn(
+          'flex h-10 w-full rounded-md border border-slate-300 bg-transparent py-2 px-3 text-sm placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-700 dark:text-slate-50 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900'
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Input.displayName = 'Input'
 
-export default Input
+export {Input}
